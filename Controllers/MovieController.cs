7f427200f1d3631b981b5ba7f0e904235cab5d32
SzymonGonerko <szymon.gonerko@gmail.com>
2023-06-18 00:15:13 +0200

    MovieController, GetAll()

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using IDBM.entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDBM.Controllers
+{
+    [Route("api/Movie")]
+    public class MovieController : ControllerBase
+    {
+        private MovieDbContext _dbContext;
+        public MovieController(MovieDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public ActionResult<IEnumerable<Movie>> GetAll()
+        {
+            var movies = _dbContext.Movies.ToList();
+            return Ok(movies);
+        }
+    }
+}
